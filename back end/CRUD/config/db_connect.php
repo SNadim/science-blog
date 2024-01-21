d4de5d9c7a31e57b973a10f675d25cfaa03b18fb
Shariar Nadim <93469458+SNadim@users.noreply.github.com>
2024-01-22 03:02:19 +0600

    Update db_connect.php

--- back end/CRUD/config/db_connect.php
+++ back end/CRUD/config/db_connect.php
@@ -2,9 +2,9 @@
 header("Access-Control-Allow-Origin: *"); // add this CORS header to enable any domain to send HTTP requests to there endpoints;
 header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
-$serverName = "localhost:3307";
-$userName = "root";
-$password = "";
+$serverName = "bt29d4uyfy9ta9416gp7-mysql.services.clever-cloud.com";
+$userName = "uuax8zeehlz5nquf";
+$password = "LQj98KlHfZ6rcfp3FW3X";
 $dbName = "db_blog";
 $id = '';
 $connection = mysqli_connect($serverName,$userName,$password,$dbName);
